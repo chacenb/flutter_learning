26b4af2ca64a1535ef86a2233da0710bc45d8f75
b.tchassem <b.tchassem@sahelys.com>
2024-02-23 11:30:31 +0100

    Working with container : Layout the canvas : OK

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -6,7 +6,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
 
-/* OLD CODE STATE ---------------------
+/* Working with some logic inside the code  ---------------------
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,6 +51,7 @@ class Dashboard extends StatelessWidget {
   getNumber() => Random().nextInt(100);
 */
 
+  /* ************************************************************************* */
   /* Adding images basics */
   /* @override
   Widget build(BuildContext context) {
@@ -76,6 +77,9 @@ class Dashboard extends StatelessWidget {
         );
   }*/
 
+  /* ************************************************************************* */
+  /* Adding icons */
+/*
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -88,7 +92,7 @@ class Dashboard extends StatelessWidget {
           // icon: Icon(Icons.alt_route_rounded,), // this is a material icon
           icon: const Icon(FontAwesomeIcons.anchor), // this is a fontawesome icon after installing the dependency
           color: Colors.red,
-          iconSize: 60.0,
+          // iconSize: 30.0,
           onPressed: () {},
           // splashColor: Colors.green,
           highlightColor: Colors.lightBlueAccent,
@@ -96,4 +100,70 @@ class Dashboard extends StatelessWidget {
       ),
     );
   }
+*/
+
+  /* ************************************************************************* */
+  /* Working with container : Layout the canvas : OK */
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          // width: 400.0,
+          height: 600.0,
+          margin: const EdgeInsets.all(8.0),
+
+          decoration: BoxDecoration(
+            color: Colors.green[100],
+            border: Border.all(color: Colors.black, width: 1.0),
+            borderRadius: const BorderRadius.all(Radius.circular(15.0)),
+          ),
+          // alignment: Alignment.center,
+          child: ListView(
+            padding: const EdgeInsets.all(10.0),
+            children: <Widget>[
+              for (int i = 0; i < 6; i += 1) // loop to render multiple widgets
+                Container(
+                    padding: const EdgeInsets.all(10.0),
+                    margin: const EdgeInsets.all(10.0),
+                    width: 200.0,
+                    height: 150.0,
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                      // color: Colors.white,
+                      image: DecorationImage(
+                        image: AssetImage("images/chacecomlogo.png"),
+                        // fit: BoxFit.fill
+                      ),
+                      border: Border.symmetric(
+                          vertical: BorderSide(color: Colors.grey, width: 3.0),
+                          horizontal:
+                              BorderSide(color: Colors.grey, width: 3.0)),
+                      // borderRadius: const BorderRadius.all(Radius.circular(15.0)),
+                      shape: BoxShape.circle,
+                      // doesn't work along w/ borderRadius
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.blueGrey,
+                            offset: Offset(2.0, 3.0),
+                            blurRadius: 13.0,
+                            spreadRadius: 1.0,
+                            blurStyle: BlurStyle.normal)
+                      ],
+                      // backgroundBlendMode: BlendMode.hardLight,
+                    ),
+                    child: Text(
+                      'Cont ${getNumber()}',
+                      style: const TextStyle(
+                          fontSize: 13.0, fontWeight: FontWeight.bold),
+                    )),
+              Text('GPS position is ${getGPSpositions()}',
+                  textAlign: TextAlign.center, style: TextStyle(fontSize: 23.0),)
+            ],
+          )),
+    );
+  }
+
+  getNumber() => Random().nextInt(100);
+
+  getGPSpositions() => Random().nextInt(100);
 }
